00bf04fc177f2c86b09198224580cade132b46da
Vetési Zoltán <vetesi.zoltan@gmail.com>
2018-04-08 13:53:51 +0200

    fix: babel

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
-require("babel-register")
+require("babel-register")({
+    cache: true,
+    ignore: function (filename) {
+        if (/@anzar\/build\//.test(filename)) {
+            return false
+        }
+        return /node_modules/.test(filename)
+    }
+})
 
 // TODO: maybe set environment variables
 
